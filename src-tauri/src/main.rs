a44031be7ac5b9e51d755eec3ca36c6d44cdb0dd
jaydenpiao <jaydenpiao@gmail.com>
2024-10-19 15:20:52 -0700

    simple rust functions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,36 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn test_command() {
+  println!("test");
+}
+
+// this function can return data
+#[tauri::command]
+fn return_command() -> String {
+  "Hello from Rust!".into()
+}
+
+// this function has error handling
+#[tauri::command]
+fn error_handling_command() -> Result<String, String> {
+  // If something fails
+  Err("This failed!".into())
+  // If it worked
+  Ok("This worked!".into())
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+  println!("inside rust code")
+  format!("hello {}!", name)
+}
+
 fn main() {
-  app_lib::run();
+  tauri::Builder::default()
+    // This is where you pass in your commands
+    .invoke_handler(tauri::generate_handler![greet])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
